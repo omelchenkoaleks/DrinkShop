fc45ed4fd9201b0401e772fec1ee5188977f93bd
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-02-21 13:59:39 +0200

    retrofit...

---
+++ app/src/main/java/com/omelchenkoaleks/drinkshop/utils/Common.java
@@ -0,0 +1,13 @@
+package com.omelchenkoaleks.drinkshop.utils;
+
+import com.omelchenkoaleks.drinkshop.retrofit.IDrinkShopAPI;
+import com.omelchenkoaleks.drinkshop.retrofit.RetrofitClient;
+
+public class Common {
+    // In Emulator, localhost = 10.0.2.2
+    private static final String BASE_URL = "http://localhost/drinkshop/";
+
+    public static IDrinkShopAPI getAPI() {
+        return RetrofitClient.getClient(BASE_URL).create(IDrinkShopAPI.class);
+    }
+}
